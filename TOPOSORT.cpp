67e34096e0a3b5cbb8007554db4fb5b7ea219490
Arik Pamnani <arikpamnani@gmail.com>
2017-06-25 00:45:51 +0530

    solved TOPOSORT.cpp

--- TOPOSORT.cpp
+++ TOPOSORT.cpp
@@ -25,40 +25,19 @@ typedef long long ll;
 const int mod = 1e9+7;
 const ll INF = 1e18;
 
-// set<int> white;
+set<int> white;
 
 int gray[10007];
-int white[10007];
 // vi TOPO;
 
 int visited[10007];
-
-int dfs(vector<vi> Adj, int s)	{
-	// white.erase(s);
-	white[s] = 0;
-	gray[s] = 1;
-	REP(j, Adj[s].size())	{
-		const bool is_in = white[Adj[s][j]];
-		int m;	
-		if(is_in == 1)	{
-			m = dfs(Adj, Adj[s][j]);
-			if(m == -1)
-				return -1;
-		}
-		if(gray[Adj[s][j]])
-			return -1;
-	}
-	gray[s] = 0;
-	// TOPO.pb(s);
-	return 0;
-}
 
 int main()	{
 	int n, m;
 	sd(n);
 	sd(m);
 	REP(i, n)
-		white[i] = 1;
+		white.insert(i);
 	vector<vi> Adj(n);
 	vi in(n);
 	REP(i, m)	{
@@ -68,20 +47,9 @@ int main()	{
 		Adj[u-1].pb(v-1);
 		in[v-1] += 1;
 	}
-	int k=0;
-	while(k < n)	{
-		if(white[k] == 1)	{
-			int x = dfs(Adj, k);
-			if(x == -1)	{
-				// cout << "Sandro fails" << endl;
-				printf("Sandro fails\n");
-				return 0;
-			}
-		}
-		k += 1;
-	}
-	// vi TOPO;
-	priority_queue<int, vector<int>, greater<int>> PQ;
+	
+	vi TOPO;
+	priority_queue<int, vector<int>, greater<int> > PQ;
 	REP(i, n)	{
 		if(in[i] == 0)	{
 			visited[i] = 1;
@@ -94,14 +62,25 @@ int main()	{
 		PQ.pop();
 		REP(j, Adj[i].size())	{
 			in[Adj[i][j]] -= 1;
+			if(in[Adj[i][j]] == -1)	{
+				printf("Sandro fails.\n");
+				return 0;
+			}
 			if(in[Adj[i][j]] == 0 && visited[Adj[i][j]] == 0)	{
 				visited[Adj[i][j]] = 1;
 				PQ.push(Adj[i][j]);
 			}
 		}
-		// TOPO.pb(i);
-		printf("%d ", i+1);
+		TOPO.pb(i);
+	}
+	if(TOPO.size() < n)	{
+		printf("Sandro fails.\n");
+		return 0;
+	}
+	REP(i, TOPO.size())	{
+		printf("%d ", TOPO[i]+1);
 	}
 	printf("\n");
 	return 0;
 }
+
